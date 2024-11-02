9549c0558a523f18051821013dde836cabbb1336
jexorDev <dustineseger@yahoo.com>
2024-11-02 13:24:15 -0500

    add vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,4 +3,11 @@
 
 <template>
   <div>I need...</div>
+  
+  <v-card>
+    <v-card-text>
+      <v-icon icon="mdi-home"></v-icon>    
+      to claim my bags
+    </v-card-text>
+    </v-card>
 </template>
