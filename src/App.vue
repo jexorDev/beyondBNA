482f80de6959c85c20a9e0e362fce63adb40bea8
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2024-11-11 11:45:17 -0600

    Hotel filter search (#8)
    * try to fix viewport
    
    * hotel search
    
    ---------
    
    Co-authored-by: jexorDev <dustineseger@yahoo.com>

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
-import {onMounted} from "vue";
-import HotelCard from './components/HotelCard.vue'
-import hotels from "./data/hotels.json";
-
-onMounted(() => {
- 
-  //console.log(JSON.parse(hotels))
-  
-})
-
+import HotelPage from "./components/HotelPage.vue";
 </script>
 
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <v-row >
-          <v-col v-for="hotel of hotels" cols="12" xs="12">
-            <HotelCard :name="hotel.Name" :address="hotel.Address" :phone="hotel.Phone" :stars="hotel.Stars" :shuttleHours="hotel.ShuttleHours" :shuttleFrequency="hotel.ShuttleFrequency" :busStop="hotel.BusStop" :busStopDistance="hotel.BusStopDistance" :busStopTime="hotel.BusStopTime" />
-          </v-col>
-        </v-row>
-        </v-container>  
+      <HotelPage/>
     </v-main>
     </v-app>
 </template>
